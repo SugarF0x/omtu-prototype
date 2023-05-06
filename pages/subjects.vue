efd0ddcb460d9760c05fdd357702adb2ce492904
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-05-06 20:17:55 +0300

    add subjects wireframe

--- pages/subjects.vue
+++ pages/subjects.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-  <h1>
-    Subjects
-  </h1>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="n in 24"
+        :key="n"
+        cols="4"
+      >
+        <v-card height="200"></v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
