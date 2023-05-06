1571a9a6db8b0c0dbb80e8afc067b709ec13450f
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-05-06 21:05:13 +0300

    ignore route specific components

---
+++ pages/schedule/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Timeline from "./components/Timeline.vue"
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          cock
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <timeline />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
