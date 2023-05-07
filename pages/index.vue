e17bd43c9c21ced8883eae089ef5abb6722a4e23
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-05-07 21:39:36 +0300

    add placeholders to map page

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
     </v-col>
 
     <v-col class="content" cols="12" sm="8">
-      <v-sheet v-for="n in 12" rounded="lg" class="pa-4">
+      <v-sheet v-for="() in 12" rounded="lg" class="pa-4">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda at commodi deserunt dolores, est ex maxime necessitatibus non, provident quo quod recusandae rerum velit, voluptas. Accusamus architecto corporis cum cupiditate doloremque ipsum libero nesciunt similique vero. Aliquam dicta facilis ipsam officia quae quibusdam quos tenetur? Ab aliquid aperiam aspernatur corporis delectus dicta ducimus et expedita labore laudantium libero obcaecati odit omnis, perferendis porro praesentium quae quis quisquam similique sunt! Adipisci aspernatur blanditiis dicta dolor dolorem dolores dolorum eaque earum esse exercitationem explicabo harum id, incidunt itaque labore nisi porro saepe sequi vel velit. Architecto beatae, culpa distinctio doloribus enim error est ex excepturi facere illum labore laboriosam mollitia nobis nulla possimus praesentium provident quae quam quas quidem reprehenderit soluta totam velit veniam voluptates. Amet illum magni officiis quia ratione tempore? Architecto aspernatur blanditiis corporis delectus dicta distinctio, dolorum eaque eum illum inventore iste necessitatibus nisi, omnis ratione repellat sed soluta. Delectus, hic laborum minus quidem quis vel. Architecto consequatur cumque doloribus expedita harum impedit inventore ipsum laborum minus, nemo nihil numquam odit officiis? Accusamus animi fugit placeat sit voluptate. Ad, at atque autem ea id incidunt ipsum, nobis numquam obcaecati pariatur perferendis, quidem quisquam sed sequi vero vitae voluptates.
       </v-sheet>
     </v-col>
--- pages/map.vue
+++ pages/map.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-
+const tab = ref(null)
 </script>
 
 <template>
-  <h1>
-    Map
-  </h1>
+  <div>
+    <h1>Интерактивная карта</h1>
+    <h4>Изображения-заглушки</h4>
+
+    <v-tabs v-model="tab">
+      <v-tab value="one">Блок А</v-tab>
+      <v-tab value="two">Блок Б</v-tab>
+    </v-tabs>
+
+    <v-window v-model="tab">
+      <v-window-item value="one">
+        <v-img src="/img/map/1.jpg" />
+      </v-window-item>
+
+      <v-window-item value="two">
+        <v-img src="/img/map/2.jpg" />
+      </v-window-item>
+    </v-window>
+  </div>
 </template>
 
 <style scoped>
