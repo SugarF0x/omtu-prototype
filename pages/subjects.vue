26742b4b876c0709180f2fe92e19299141fa7890
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-05-07 20:28:53 +0300

    render subjects from dummy data

--- pages/subjects.vue
+++ pages/subjects.vue
@@ -1,21 +1,38 @@
 <script setup lang="ts">
-
+import data from "~/assets/dummy_schedule"
 </script>
 
 <template>
   <v-container>
     <v-row>
       <v-col
-        v-for="n in 24"
-        :key="n"
+        v-for="subject in data.subjects"
+        :key="subject.id"
         cols="4"
+        class="col"
       >
-        <v-card height="200"></v-card>
+        <v-card self-stretch>
+          <v-img
+            :src="`http://placehold.it/600x300/${subject.color.replace('#', '')}/ffffff.jpg&text=${subject.title}`"
+            width="600"
+            aspect-ratio="2"
+          />
+
+          <v-card-title>
+            {{ subject.title }}
+          </v-card-title>
+
+          <v-card-text>
+            {{ subject.professor }}
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.col {
+  display: flex;
+}
 </style>
