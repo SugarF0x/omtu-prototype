9a9333cf29abdbb11c0d6a1f2b80c8607ce243a7
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-05-08 19:40:53 +0300

    add user profile card

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,13 +1,62 @@
 <script setup lang="ts">
+import useUserStore from "~/stores/user"
 
+const { name } = storeToRefs(useUserStore())
 </script>
 
 <template>
-  <div>
+  <v-container>
+    <v-row>
+      <v-col cols="4" class="column">
+        <v-card class="profile-card">
+          <v-card-title class="title">
+            <v-avatar class="hidden-sm-and-down" color="grey-darken-1" size="128" image="/img/avatar.jpg" />
+            <div class="text-h4">
+              {{ name }}
+            </div>
+          </v-card-title>
 
-  </div>
+          <v-card-subtitle class="text-center">
+            ОМТУ I
+          </v-card-subtitle>
+
+          <v-card-text class="text-center">
+            Digital-маркетинг
+          </v-card-text>
+        </v-card>
+
+        <v-card>
+          ROLES
+        </v-card>
+      </v-col>
+
+      <v-col cols="8" class="column">
+        <v-card>
+          INFO
+        </v-card>
+
+        <v-card>
+          GRADES
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped lang="scss">
+.column {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
 
+.profile-card {
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+  }
+}
 </style>
