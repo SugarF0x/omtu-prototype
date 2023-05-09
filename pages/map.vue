ebbd9f56d0e538b904b28aa466310cbb6c37e09a
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-05-09 21:39:53 +0300

    add a new image to map section

--- pages/map.vue
+++ pages/map.vue
@@ -10,6 +10,7 @@ const tab = ref(null)
     <v-tabs v-model="tab">
       <v-tab value="one">Блок А</v-tab>
       <v-tab value="two">Блок Б</v-tab>
+      <v-tab value="three">Блок В</v-tab>
     </v-tabs>
 
     <v-window v-model="tab">
@@ -20,7 +21,15 @@ const tab = ref(null)
       <v-window-item value="two">
         <v-img src="/img/map/2.jpg" />
       </v-window-item>
+
+      <v-window-item value="three">
+        <v-img src="/img/map/3.jpg" />
+      </v-window-item>
     </v-window>
+
+    <div class="text-right mt-2">
+      Навигационный <a href="https://t.me/MGIMO_mapbot" target="_blank">Телеграмм Бот</a>
+    </div>
   </div>
 </template>
 
